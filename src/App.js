7fb3bdc07de4434104be5ab1bd2dd92fddfa0910
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-11-27 18:56:35 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+import {Provider} from './context';
 
 import './App.css';
 
 class App extends Component {
   render(){
     return(
+     <Provider>
       <Router>
         <React.Fragment>
           <Navbar />
@@ -18,6 +22,7 @@ class App extends Component {
           </div>
         </React.Fragment>
        </Router>
+      </Provider>
     );
   }
 }
