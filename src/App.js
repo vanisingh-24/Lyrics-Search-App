5f2ecd4a0d4623d9f8513946e0876a1f40771aca
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-11-28 15:51:36 +0530

    Lyrics route Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
 
 import {Provider} from './context';
 
@@ -18,6 +19,7 @@ class App extends Component {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
